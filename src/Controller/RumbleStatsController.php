dee59da0345b689bf67719cc0944623c1d1e5194
Maciej Łebkowski <m.lebkowski@gmail.com>
2017-06-08 16:43:43 +0200

    require valid factions as well, fix rumble being optional

--- src/Controller/RumbleStatsController.php
+++ src/Controller/RumbleStatsController.php
@@ -2,17 +2,18 @@
 
 namespace Nassau\CartoonBattle\Controller;
 
+use Nassau\CartoonBattle\Entity\Guild\Guild;
 use Nassau\CartoonBattle\Entity\Rumble\Rumble;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class RumbleStatsController extends Controller
 {
-    public function csvAction(Rumble $rumble = null, $factionId)
+    public function csvAction(Guild $guild, Rumble $rumble = null)
     {
         $handle = fopen('php://memory', 'w');
 
-        $stats = $this->get('cartoon_battle.guild_stats')->getStats($rumble, $factionId);
+        $stats = $this->get('cartoon_battle.guild_stats')->getStats($rumble, $guild->getFactionId());
 
         foreach ($stats as $user) {
             $points = 0 === end($user['points']) ? array_slice($user['points'], 0, -1) : $user['points'];
