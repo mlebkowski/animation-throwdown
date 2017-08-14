89a3baf3f44b922dc3fd781bbe466510a83b96d8
Maciej Łebkowski <m.lebkowski@gmail.com>
2017-08-14 13:07:12 +0200

    remove some bloated fields from the results

--- src/Services/Farming/Chores/AbstractBattleChore.php
+++ src/Services/Farming/Chores/AbstractBattleChore.php
@@ -41,7 +41,7 @@ abstract class AbstractBattleChore implements FarmingChore
 
             $winner = (bool)$result['battle_data']['winner'];
 
-            $configuration->addResult($nextTarget, $winner, $result);
+            $configuration->addResult($nextTarget, $winner, $this->stripResult($result));
 
             $loot = $winner ? $this->lootExtractor->extractLoot($result) : null;
 
@@ -87,6 +87,19 @@ abstract class AbstractBattleChore implements FarmingChore
         }
 
         $logWriter("");
+    }
+
+    private function stripResult(array $result)
+    {
+        return array_diff_key($result, [
+            'active_events' => false,
+            'item_data' => false,
+            'current_missions' => false,
+            'mission_completions' => false,
+            'pvp_ranks' => false,
+            'store_items' => false,
+            'user_units' => false,
+        ]);
     }
 
 }
