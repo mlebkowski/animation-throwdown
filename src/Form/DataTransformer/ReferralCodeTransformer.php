4933c4593126b7383ba08cbc6240f21ee14514fc
Maciej Łebkowski <m.lebkowski@gmail.com>
2018-04-30 13:56:27 +0200

    allow empty farming codes

--- src/Form/DataTransformer/ReferralCodeTransformer.php
+++ src/Form/DataTransformer/ReferralCodeTransformer.php
@@ -29,6 +29,6 @@ class ReferralCodeTransformer implements DataTransformerInterface
 
     public function reverseTransform($value)
     {
-        return $this->repository->findOneBy(['name' => $value, 'enabled' => 1]);
+        return $this->repository->findOneBy(['name' => (string)$value, 'enabled' => 1]);
     }
 }
